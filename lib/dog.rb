21db0c64ddfab7f2306a25232395f2762a811816
Philip Lee-Chin <phil.leechin@outlook.com>
2020-04-25 18:02:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,2 @@
-class Dog
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/students.sqlite"
-  )
-
-  ActiveRecord::Base.connection.execute(sql)
+class Dog < ActiveRecord::Base
 end
